e461ce3a854c16f9afc9f3f2037a1dcf6085aa8a
Gage Langdon <Gage Langdon>
2016-12-14 22:50:13 -0800

    merged with local stashed items

--- camera/routes/camera.js
+++ camera/routes/camera.js
@@ -1,3 +1,4 @@
+
 var parse_license = require('../utilities/parse_license.js');
 var nodeWebcam = require('node-webcam');
 plates = [];
@@ -26,4 +27,26 @@ function getLicensePlate(image, state, country, status) {
 exports.getLicensePlate = getLicensePlate;
 
 
+var parse_license = require('../utilities/parse_license.js');
+plates = [];
 
+function getLicensePlate(image, state, country, status){
+
+    if (status == 'entering'){
+        plate = parse_license.parsePlate();
+        plates.push(plate);
+    }
+    else{
+        if (plates.length > 0)
+            plate = plates.shift();
+        else
+            return {error: 'Error: No cars in system to exit.'};
+    }
+
+    return data = {
+        state: state,
+        country: country,
+        id: plate
+    }
+}
+exports.getLicensePlate = getLicensePlate;
